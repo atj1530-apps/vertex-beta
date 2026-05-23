150284d7bb6d2630c18450e8c3c2819ec6da4db3
atj1530-apps <atj1530@gmail.com>
2026-05-22 20:07:54 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Bump CACHE_NAME on every deploy to bust old caches.
    Network-first for HTML so new deploys are always visible immediately. */
 
-const CACHE_NAME = 'vertex-v20260522-k';
+const CACHE_NAME = 'vertex-v20260522-l';
 const APP_SHELL = [
   './',
   './index.html',

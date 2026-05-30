912dd94bae7b2b9774e99a7906b8fb5c2109d915
atj1530-apps <atj1530@gmail.com>
2026-05-29 22:51:37 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    v20260529-6b2
    Network-first for HTML so deploys are visible immediately.
 */
-const CACHE_NAME = 'vertex-v20260529-6a5-category-cleanup';
+const CACHE_NAME = 'vertex-v20260529-6a7-filter-engine';
 const APP_SHELL = [
   './',
   './index.html',

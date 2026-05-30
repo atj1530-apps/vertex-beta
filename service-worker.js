6fee892aa8ac180d05b8d1088fee6acec11430dd
atj1530-apps <atj1530@gmail.com>
2026-05-30 14:53:36 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    v20260529-6b2
    Network-first for HTML so deploys are visible immediately.
 */
-const CACHE_NAME = 'vertex-v20260530-6a11d-safe-tooltip-equipment';
+const CACHE_NAME = 'vertex-v20260530-6a11f-sidebar-narrowsafe-tooltip-equipment';
 const APP_SHELL = [
   './',
   './index.html',
